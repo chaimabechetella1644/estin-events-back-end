408316605d663e1aa66452822673fad3ca74c445
Aya LAMIRI <119955280+AyaLamiri@users.noreply.github.com>
2025-12-02 16:47:58 +0100

    to use MongoClient

--- config/db.js
+++ config/db.js
@@ -1,14 +1,22 @@
-const mongoose = require('mongoose');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI); 
-    console.log("MongoDB Connected!");
-  } catch (err) {
-    console.error("MongoDB Connection Error:", err.message);
-    process.exit(1);
-  }
+const client = new MongoClient(process.env.MONGO_URI);
+let db;
+
+const connectDB = () => {
+  return client.connect()
+    .then(() => {
+      db = client.db("ClubsEvents");
+      console.log("MongoDB Connected!");
+      return db;
+    })
+    .catch(err => {
+      console.error("MongoDB Connection Error:", err.message);
+      process.exit(1);
+    });
 };
 
-module.exports = connectDB;
+const getDB = () => db;
+
+module.exports = { connectDB, getDB };
